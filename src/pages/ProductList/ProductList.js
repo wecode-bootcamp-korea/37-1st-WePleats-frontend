87309b9b62e2484db13edea6e361ce06ba011c6a
rayong <skdyds@gmail.com>
2022-09-20 08:59:21 +0900

    add : 리스트 페이지 4단 컬럼 구조 완성

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,5 +1,136 @@
+import React from 'react';
+import './ProductList.scss';
+
 function ProductList() {
-  return <div>ProductList</div>;
+  return (
+    <div className="productlist">
+      <div className="outerBox">
+        <div className="filterBox">filter</div>
+        <ul className="listBox">
+          <li className="cardBox">
+            <figure
+              className="img"
+              style={{
+                background:
+                  'url(https://velog.velcdn.com/images/rayong/post/5516bca5-2764-4357-b710-544fc7b21cf7/image.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <div className="desc">
+              <p className="name">키보드</p>
+              <p className="price">59,000원</p>
+            </div>
+          </li>
+          <li className="cardBox">
+            <figure
+              className="img"
+              style={{
+                background:
+                  'url(https://velog.velcdn.com/images/rayong/post/5516bca5-2764-4357-b710-544fc7b21cf7/image.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <div className="desc">
+              <p className="name">키보드</p>
+              <p className="price">59,000원</p>
+            </div>
+          </li>
+          <li className="cardBox">
+            <figure
+              className="img"
+              style={{
+                background:
+                  'url(https://velog.velcdn.com/images/rayong/post/5516bca5-2764-4357-b710-544fc7b21cf7/image.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <div className="desc">
+              <p className="name">키보드</p>
+              <p className="price">59,000원</p>
+            </div>
+          </li>
+          <li className="cardBox">
+            <figure
+              className="img"
+              style={{
+                background:
+                  'url(https://velog.velcdn.com/images/rayong/post/5516bca5-2764-4357-b710-544fc7b21cf7/image.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <div className="desc">
+              <p className="name">키보드</p>
+              <p className="price">59,000원</p>
+            </div>
+          </li>
+          <li className="cardBox">
+            <figure
+              className="img"
+              style={{
+                background:
+                  'url(https://velog.velcdn.com/images/rayong/post/5516bca5-2764-4357-b710-544fc7b21cf7/image.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <div className="desc">
+              <p className="name">키보드</p>
+              <p className="price">59,000원</p>
+            </div>
+          </li>
+          <li className="cardBox">
+            <figure
+              className="img"
+              style={{
+                background:
+                  'url(https://velog.velcdn.com/images/rayong/post/5516bca5-2764-4357-b710-544fc7b21cf7/image.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <div className="desc">
+              <p className="name">키보드</p>
+              <p className="price">59,000원</p>
+            </div>
+          </li>
+          <li className="cardBox">
+            <figure
+              className="img"
+              style={{
+                background:
+                  'url(https://velog.velcdn.com/images/rayong/post/5516bca5-2764-4357-b710-544fc7b21cf7/image.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <div className="desc">
+              <p className="name">키보드</p>
+              <p className="price">59,000원</p>
+            </div>
+          </li>
+          <li className="cardBox">
+            <figure
+              className="img"
+              style={{
+                background:
+                  'url(https://velog.velcdn.com/images/rayong/post/5516bca5-2764-4357-b710-544fc7b21cf7/image.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <div className="desc">
+              <p className="name">키보드</p>
+              <p className="price">59,000원</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default ProductList;
