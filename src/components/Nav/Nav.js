b968f9876babc5e7b13a0ac7b0774207078a3dea
gamangee <gamangee1991@gmail.com>
2022-09-19 22:34:24 +0900

    ADD : navbar 작업진행중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,38 @@
+import React from 'react';
+import './Nav.scss';
+
 function Nav() {
-  return <div></div>;
+  return (
+    <nav className="navBox">
+      <div className="nav">
+        <div className="menu">
+          <div className="logo">WePleats</div>
+          <div className="best">Best</div>
+          <div className="new">New</div>
+          <div className="electronic">Electronic</div>
+          <div className="kitchen">Kitchen</div>
+          <div className="etc">Etc</div>
+        </div>
+        <div className="userInfo">
+          <div className="name">장문정</div>
+          <div className="line" />
+          <div className="myPage">
+            <i className="fa-solid fa-user" />
+          </div>
+          <div className="line" />
+          <div className="cart">
+            <i className="fa fa-shopping-bag" />
+            <div className="count">3</div>
+          </div>
+          <div className="line" />
+          <div className="search">
+            <i className="fa-solid fa-magnifying-glass" />
+          </div>
+        </div>
+      </div>
+      {/* <div className="navDropDown"></div> */}
+    </nav>
+  );
 }
 
 export default Nav;
