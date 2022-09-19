13e124c7d34c15f33dacc503d204b7af64b70697
JuwonChoi902 <qfnl90@naver.com>
2022-09-19 20:12:57 +0900

    ADD:최주원  main 페이지 작업중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,33 @@
+import React from 'react';
+import './Main.scss';
+
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="outerBox">
+      <div className="couponBox"></div>
+      <div className="contentsBox">
+        <div className="upperBox">
+          <div className="spaceBox"></div>
+          <div className="mainPicBox"></div>
+          <div className="spaceBox"></div>
+        </div>
+        <div className="middleUppperBox">
+          <div className="spaceBox"></div>
+          <div className="productSlideBox"></div>
+          <div className="spaceBox"></div>
+        </div>
+        <div className="middleBottomBox">
+          <div className="spaceBox"></div>
+          <div className="mainStoryBox"></div>
+          <div className="spaceBox"></div>
+        </div>
+        <div className="bottomBox">
+          <div className="mainPicBox"></div>
+          <div className="spaceBox"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
