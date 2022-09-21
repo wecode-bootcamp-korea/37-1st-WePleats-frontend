3464aa041832792b9e2d9165d0dc2dad764c6448
rayong <skdyds@gmail.com>
2022-09-21 13:15:30 +0900

    Add : 로그인 페이지 레이아웃 디자인 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,10 +3,37 @@ import './Login.scss';
 
 function Login() {
   return (
-    <div className="loginBox">
-      <form className="inputBox">
-        <input className="userInput" name="id" type="text" />
-      </form>
+    <div className="login">
+      <div className="loginBox">
+        <p className="loginTitle">welcome, wepleats</p>
+        <form className="inputBox">
+          <input
+            className="userInput"
+            name="id"
+            type="text"
+            placeholder="이메일"
+            autoComplete="username"
+          />
+          <input
+            className="userInput"
+            name="pw"
+            type="password"
+            placeholder="비밀번호"
+            autoComplete="current-password"
+          />
+          <div className="checkBox">
+            <input type="checkbox" id="ch1" />
+            <label className="checkLogin" for="ch1">
+              로그인 상태 유지
+            </label>
+          </div>
+          <button className="loginBtn">로그인</button>
+        </form>
+        <div className="linkBox">
+          <p className="signupLink">회원가입</p>
+          <p className="findLink">아아디 비밀번호 찾기</p>
+        </div>
+      </div>
     </div>
   );
 }
