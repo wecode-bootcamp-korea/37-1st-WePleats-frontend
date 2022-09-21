1e3ddb4f0d5854ad2c19eae5c486c72cc24704ba
rayong <skdyds@gmail.com>
2022-09-21 13:44:12 +0900

    Add : 간단한 유효성 검사 추가 - 아이디에 @ 가 들어가고, 비밀번호가 5자기 이상일 경우 로그인 버튼 활성화 - 활성화 이후 로그인 버튼 클릭시 메인페이지 이동

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
+  const [userInput, setUserInput] = useState({
+    id: '',
+    pw: '',
+  });
+
+  const handleInput = e => {
+    const { name, value } = e.target;
+    setUserInput({ ...userInput, [name]: value });
+  };
+
+  const isValidId = userInput.id.includes('@');
+  const isValidPw = userInput.pw.length >= 5;
+  const isAllValid = isValidId && isValidPw;
+
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/');
+  };
+
   return (
     <div className="login">
       <div className="loginBox">
         <p className="loginTitle">welcome, wepleats</p>
         <form className="inputBox">
           <input
+            onChange={handleInput}
             className="userInput"
             name="id"
             type="text"
@@ -15,6 +36,7 @@ function Login() {
             autoComplete="username"
           />
           <input
+            onChange={handleInput}
             className="userInput"
             name="pw"
             type="password"
@@ -23,11 +45,17 @@ function Login() {
           />
           <div className="checkBox">
             <input type="checkbox" id="ch1" />
-            <label className="checkLogin" for="ch1">
+            <label className="checkLogin" htmlFor="ch1">
               로그인 상태 유지
             </label>
           </div>
-          <button className="loginBtn">로그인</button>
+          <button
+            onClick={goToMain}
+            disabled={!isAllValid}
+            className="loginBtn"
+          >
+            로그인
+          </button>
         </form>
         <div className="linkBox">
           <p className="signupLink">회원가입</p>
