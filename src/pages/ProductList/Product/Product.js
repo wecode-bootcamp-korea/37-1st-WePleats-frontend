6048bc2f94ed1d44bba11b990cbe3b8ce9f8dbbc
gamangee <gamangee1991@gmail.com>
2022-09-21 17:30:14 +0900

    ADD:최신화

--- src/pages/ProductList/Product/Product.js
+++ src/pages/ProductList/Product/Product.js
@@ -12,11 +12,14 @@ function Product() {
   useEffect(() => {
     fetch('data/product.json')
       .then(res => res.json())
-      .then(data => setProduct(data));
+      .then(data => {
+        setProduct(data);
+        setCurrentUrl(data.img[0].imageUrl);
+      });
   }, []);
 
   return (
-    <>
+    <div className="productBox">
       <nav className="nav">
         <ul className="subMenu">
           <li className="li home">Home</li>
@@ -35,12 +38,12 @@ function Product() {
           <div className="main">
             <img className="img" src={currentUrl} alt="메인 이미지" />
           </div>
-          <div className="sub" onMouseOver={getUrl}>
+          <div className="sub">
             <ul className="subImgs">
               {product.img &&
                 product.img.map(item => {
                   return (
-                    <li key={item.id} className="subImg">
+                    <li onMouseOver={getUrl} key={item.id} className="subImg">
                       <img
                         className="img"
                         src={item.imageUrl}
@@ -52,8 +55,37 @@ function Product() {
             </ul>
           </div>
         </section>
+        <section className="form">
+          <div className="header">
+            <div className="title">
+              <h1 className="name">item.name</h1>
+              <h1 className="price">item.price</h1>
+            </div>
+            <div className="share">
+              <i className="fa-solid fa-share-nodes" />
+            </div>
+          </div>
+          <div className="colors" />
+          <ul className="options">
+            <li className="option">
+              <strong>배송방법</strong>택배
+            </li>
+            <li className="option">
+              <strong>배송비</strong>무료
+              {/* {item.price >= 50000
+                ? '무료 (50,000원 이상 무료배송) |도서산간 배송비 추가'
+                : ' 3,000원 (50,000원 이상 무료배송) |도서산간 배송비 추가'} */}
+            </li>
+          </ul>
+          <div className="quantity">
+            <h1>수량</h1>
+            <div className="border" />
+          </div>
+          <div className="total" />
+          <div className="buttons" />
+        </section>
       </main>
-    </>
+    </div>
   );
 }
 
