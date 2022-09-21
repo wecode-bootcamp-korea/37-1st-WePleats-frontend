5dedd6a3119961d4e255e435bf8a6cf77903fe94
JuwonChoi902 <qfnl90@naver.com>
2022-09-21 21:06:28 +0900

    ADD: 최주원 메인페이지 레이아웃, 슬라이드 기능 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,64 +2,158 @@ import React, { useEffect, useState } from 'react';
 import './Main.scss';
 
 function Main() {
-  const [position, setPosition] = useState(0);
-  const [clicked, setClicked] = useState(0);
-  const slideLeft = () => {
-    setPosition(current => current + 25);
-    setClicked(current => current + 1);
-  };
-
-  const slideRight = () => {
-    setPosition(current => current - 25);
-    setClicked(current => current + 1);
-  };
-
-  // const intervalSlide = () => {
-
-  // };
-
-  // const clearSlideInterval = clearInterval(intervalSlide);
+  //** 상단 쿠폰bar 무빙기능 **
+  const [vwForCoupon, setVwForCoupon] = useState(0);
+  const [animateForCoupon, setAnimateForCoupon] = useState(' animate');
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setPosition(current => current - 25);
-      console.log('5초가 지났습니다');
-    }, 5000);
-    // intervalSlide();
+      setVwForCoupon(current => current + 100);
+    }, 2100);
 
     return () => clearInterval(interval);
-  }, [clicked]);
-  console.log(position);
-  // const timeForCarusel = () => {
-  //   setInterval(() => {
-  //     setPosition(current => current - 25);
-  //   }, 3000);
-  //   console.log(position);
-  // };
+  }, [vwForCoupon]);
 
-  // timeForCarusel();
+  const couponVwValue = vwForCoupon;
+
+  if (couponVwValue === 100) {
+    setTimeout(() => {
+      setAnimateForCoupon('');
+      setVwForCoupon(0);
+    }, 2000);
+    setTimeout(() => {
+      setAnimateForCoupon(' animate');
+    }, 2100);
+  }
+
+  //** 상단 슬라이드 이미지 **
+  const [indexForUpper, setIndexForUpper] = useState(0);
+  const [animateForUpper, setAnimateForUpper] = useState(' animate');
+
+  const upperSlideLeft = () => {
+    setIndexForUpper(current => current - 1);
+  };
+
+  const upperSlideRight = () => {
+    setIndexForUpper(current => current + 1);
+  };
+
+  const moveUpperSlide = indexForUpper * 41;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndexForUpper(current => current + 1);
+    }, 6000);
+    return () => clearInterval(interval);
+  }, [indexForUpper]);
+
+  if (
+    indexForUpper === mok_Main_Best.length ||
+    indexForUpper === -mok_Main_Best.length
+  ) {
+    setTimeout(() => {
+      setAnimateForUpper('');
+      setIndexForUpper(0);
+    }, 600);
+
+    setTimeout(() => {
+      setAnimateForUpper(' animate');
+    }, 700);
+  }
+
+  //** 아랫쪽 슬라이드 이미지 **
+  const [indexForLower, setIndexForLower] = useState(0);
+  const [animateForLower, setAnimateForLower] = useState(' animate');
+
+  const lowerSlideLeft = () => {
+    setIndexForLower(current => current - 1);
+  };
+
+  const lowerSlideRight = () => {
+    setIndexForLower(current => current + 1);
+  };
+
+  const moveLowerSlide = indexForLower * 25;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndexForLower(current => current + 1);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, [indexForLower]);
+
+  if (
+    indexForLower === mok_Main_New.length ||
+    indexForLower === -mok_Main_New.length
+  ) {
+    setTimeout(() => {
+      setAnimateForLower('');
+      setIndexForLower(0);
+    }, 600);
+
+    setTimeout(() => {
+      setAnimateForLower(' animate');
+    }, 700);
+  }
 
   return (
     <div className="outerBox">
-      <div className="couponBox"></div>
+      <div className="couponBox">
+        <div
+          className={`movingTextBox${animateForCoupon}`}
+          style={{ transform: `translate(${vwForCoupon}vw)` }}
+        >
+          <div className="movingText1">누르면 쿠폰지급^^</div>
+          <div className="movingText2">누르면 쿠폰지급^^</div>
+        </div>
+      </div>
       <div className="contentsBox">
         <div className="upperBox">
-          <div className="spaceBox"></div>
+          <div className="spaceBox" />
           <div className="mainPicBox">
             <div className="newestItem">
-              <div className="pictureBox1">
-                <img
-                  className="mainPic"
-                  src="/images/sample1.jpeg"
-                  alt="newest"
-                ></img>
+              <div
+                className={`upperSlideBox${animateForUpper}`}
+                style={{
+                  transform: `translate(${
+                    -mok_Main_Best.length * 41 - moveUpperSlide
+                  }vw)`,
+                }}
+              >
+                {mok_Main_Best.map(el => (
+                  <div className="pictureBox1 copied" key={el.id}>
+                    <img className="mainPic" src={el.imgURL2} alt="newest" />
+                  </div>
+                ))}
+                {mok_Main_Best.map(el => (
+                  <div className="pictureBox1" key={el.id}>
+                    <img className="mainPic" src={el.imgURL2} alt="newest" />
+                  </div>
+                ))}
+                {mok_Main_Best.map(el => (
+                  <div className="pictureBox1 copied" key={el.id}>
+                    <img className="mainPic" src={el.imgURL2} alt="newest" />
+                  </div>
+                ))}
               </div>
+              <img
+                className="upperSlideBtnLeft"
+                onClick={upperSlideLeft}
+                src="/images/arrowLeftWhite.png"
+                alt="arrow"
+              />
+              <img
+                className="upperSlideBtnRight"
+                onClick={upperSlideRight}
+                src="/images/arrowRightWhite.png"
+                alt="arrow"
+              />
               <div className="mainPicTextBox">
                 <img
                   className="direction"
                   src="/images/right-direction.png"
                   alt="direction"
-                ></img>
+                />
                 <span> 2022 FW 1st Drop, "The Daily Traveler"</span>
               </div>
             </div>
@@ -69,42 +163,46 @@ function Main() {
                   className="mainPic"
                   src="/images/sample2.jpeg"
                   alt="best"
-                ></img>
+                />
               </div>
               <div className="mainPicTextBox">
                 <img
                   className="direction"
                   src="/images/right-direction.png"
                   alt="direction"
-                ></img>
+                />
                 <span> 꾸준히 사랑받는 베스트 아이템 확인하기</span>
               </div>
             </div>
           </div>
-          <div className="spaceBox"></div>
+          <div className="spaceBox" />
         </div>
         <div className="middleUpperBox">
           <img
-            className="slideBtnLeft"
-            onClick={slideLeft}
+            className="lowerSlideBtnLeft"
+            onClick={lowerSlideLeft}
             src="/images/arrowLeft.png"
             alt="arrowBtn"
           />
           <img
-            className="slideBtnRight"
-            onClick={slideRight}
+            className="lowerSlideBtnRight"
+            onClick={lowerSlideRight}
             src="/images/arrowRight.png"
             alt="arrowBtn"
           />
-          <div className="spaceBox"></div>
+          <div className="spaceBox" />
           <div
-            className="productSlideBox"
-            style={{ transform: `translate(${position}vw)` }}
+            className={`productSlideBox${animateForLower}`}
+            style={{
+              transform: `translate(${
+                -mok_Main_New.length * 25 - moveLowerSlide
+              }vw)`,
+            }}
           >
             {mok_Main_New.map(el => (
-              <div className="slideContents" key={el.id}>
+              <div className="slideContents copied" key={el.id}>
                 <div className="slideImageBox">
-                  <img src={el.imgURL1} alt="sample"></img>
+                  <img src={el.imgURL1} alt="sample" />
                 </div>
                 <div className="slideTextBox">
                   <div className="slideProductName">{el.productName}</div>
@@ -115,7 +213,18 @@ function Main() {
             {mok_Main_New.map(el => (
               <div className="slideContents" key={el.id}>
                 <div className="slideImageBox">
-                  <img src={el.imgURL1} alt="sample"></img>
+                  <img src={el.imgURL1} alt="sample" />
+                </div>
+                <div className="slideTextBox">
+                  <div className="slideProductName">{el.productName}</div>
+                  <div className="slideProductPrice">{el.price}</div>
+                </div>
+              </div>
+            ))}
+            {mok_Main_New.map(el => (
+              <div className="slideContents copied" key={el.id}>
+                <div className="slideImageBox">
+                  <img src={el.imgURL1} alt="sample" />
                 </div>
                 <div className="slideTextBox">
                   <div className="slideProductName">{el.productName}</div>
@@ -124,10 +233,10 @@ function Main() {
               </div>
             ))}
           </div>
-          <div className="spaceBox"></div>
+          <div className="spaceBox" />
         </div>
         <div className="middleBottomBox">
-          <div className="spaceBox"></div>
+          <div className="spaceBox" />
           <div className="mainPicBox">
             <div className="newestItem">
               <div className="pictureBox1">
@@ -135,14 +244,14 @@ function Main() {
                   className="mainPic"
                   src="/images/sample1.jpeg"
                   alt="newest"
-                ></img>
+                />
               </div>
               <div className="mainPicTextBox">
                 <img
                   className="direction"
                   src="/images/right-direction.png"
                   alt="direction"
-                ></img>
+                />
                 <span> 새로운 계절을 기다리는 설렘</span>
               </div>
             </div>
@@ -152,40 +261,40 @@ function Main() {
                   className="mainPic"
                   src="/images/sample2.jpeg"
                   alt="best"
-                ></img>
+                />
               </div>
               <div className="mainPicTextBox">
                 <img
                   className="direction"
                   src="/images/right-direction.png"
                   alt="direction"
-                ></img>
+                />
                 <span> 러브에디션</span>
               </div>
             </div>
           </div>
-          <div className="spaceBox"></div>
+          <div className="spaceBox" />
         </div>
         <div className="bottomBox">
           <div className="eventContentsBox">
             <div className="eventInsideBox">
               <div className="innerContentsBox">
-                <img src="/images/sample1.jpeg" alt="sample"></img>
+                <img src="/images/sample1.jpeg" alt="sample" />
                 <div>OCEAN EDITION</div>
               </div>
               <div className="innerContentsBox">
-                <img src="/images/sample1.jpeg" alt="sample"></img>
+                <img src="/images/sample1.jpeg" alt="sample" />
                 <div>PROCESS OF PLASTIC TURNING INTO PLEATSMAMA</div>
               </div>
               <div className="innerContentsBox">
-                <img src="/images/sample1.jpeg" alt="sample"></img>
+                <img src="/images/sample1.jpeg" alt="sample" />
                 <div>PLMA_LAb edition</div>
               </div>
             </div>
           </div>
-          <div className="spaceBox"></div>
+          <div className="spaceBox" />
         </div>
-        <div className="footerSpaceBox"></div>
+        <div className="footerSpaceBox" />
       </div>
     </div>
   );
@@ -292,6 +401,10 @@ const mok_Main_New = [
 ];
 
 const mok_Main_Best = [
-  { id: 3, productName: 'bad', imgURL1: '', imgURL2: '' },
-  { id: 4, productName: 'soso', imgURL1: '', imgURL2: '' },
+  { id: 1, productName: 'bad', imgURL1: '', imgURL2: '/images/sample1.jpeg' },
+  { id: 2, productName: 'bad', imgURL1: '', imgURL2: '/images/sample2.jpeg' },
+  { id: 3, productName: 'bad', imgURL1: '', imgURL2: '/images/sample12.jpeg' },
+  { id: 4, productName: 'bad', imgURL1: '', imgURL2: '/images/sample4.jpeg' },
+  { id: 5, productName: 'bad', imgURL1: '', imgURL2: '/images/sample5.jpeg' },
+  { id: 6, productName: 'bad', imgURL1: '', imgURL2: '/images/sample6.jpeg' },
 ];
