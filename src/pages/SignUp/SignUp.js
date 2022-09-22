18eba03c1184e0bbfd275bf30d7ce7099e903564
rayong <skdyds@gmail.com>
2022-09-22 13:02:14 +0900

    Add : signup 페이지 레이아웃 디자인 완료

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -12,54 +12,98 @@ function SignUp() {
             </div>
           </div>
         </div>
+        {/* 이메일 비밀번호 입력 */}
         <input
-          className="userInputEmail"
+          className="userInputEmail input"
           name="id"
           type="text"
           placeholder="이메일"
           autoComplete="username"
         />
         <input
-          className="userInputPw"
+          className="userInputPw input"
           name="pw"
           type="password"
           placeholder="비밀번호"
           autoComplete="current-password"
         />
         <input
-          className="userInputPwCheck"
+          className="userInputPwCheck input"
           name="pw"
           type="password"
           placeholder="비밀번호 확인"
           autoComplete="current-password"
         />
-        <p className="userName">이름</p>
+        {/* 이름 입력 */}
+        <p className="userName title mustInput">이름</p>
         <input
-          className="userInputName"
+          className="userInputName input"
           name="name"
           type="text"
           placeholder="이름을(를) 입력하세요"
           autoComplete="username"
         />
-        <p className="userGender">성별</p>
-        <label className="userMale">
-          <input className="radio" name="gender" type="radio" />
+        {/* 성별 입력 */}
+        <p className="userGender title mustInput">성별</p>
+        <label className="userMale label">
+          <input className="radio" name="gender" type="radio" value="male" />
           <span className="text">남자</span>
         </label>
-        <label className="userFemale">
-          <input className="radio" name="gender" type="radio" />
+        <label className="userFemale label">
+          <input className="radio" name="gender" type="radio" value="female" />
           <span className="text">여자</span>
         </label>
-        <p className="userPhoneNum">휴대폰</p>
+        {/* 휴대폰 입력 */}
+        <p className="userPhoneNum title mustInput">휴대폰</p>
         <input
-          className="userInputNumber"
+          className="userInputNumber input"
           name="phoneNum"
           type="text"
-          placeholder="휴대폰"
+          placeholder="000-0000-0000 형식으로 입력하세요"
           autoComplete="username"
         />
-        <p className="userBirth">생년월일</p>
-        <p className="userDataSave">개인정보 유효기간</p>
+        {/* 생년월일 입력 */}
+        <div className="userBirth">
+          <p className="title mustInput">생년월일</p>
+          <div className="selectBox">
+            <select className="select" name="year">
+              <option value="1990">1990</option>
+              <option value="1991">1991</option>
+              <option value="1992">1992</option>
+            </select>
+            <select className="select" name="month">
+              <option value="1">1월</option>
+              <option value="2">2월</option>
+              <option value="3">3월</option>
+            </select>
+            <select className="select" name="day">
+              <option value="1">1일</option>
+              <option value="2">2일</option>
+              <option value="3">3일</option>
+            </select>
+          </div>
+        </div>
+        {/* 개인정보 유효기간 */}
+        <div className="userDataSave">
+          <p className="name title">개인정보 유효기간</p>
+          <label className="one label">
+            <input className="radio" name="time" type="radio" value="1" />
+            <span className="text">1년</span>
+          </label>
+          <label className="two label">
+            <input className="radio" name="time" type="radio" value="2" />
+            <span className="text">2년</span>
+          </label>
+          <label className="five label">
+            <input className="radio" name="time" type="radio" value="3" />
+            <span className="text">5년</span>
+          </label>
+          <label className="out label">
+            <input className="radio" name="time" type="radio" value="exit" />
+            <span className="text">회원 탈퇴 시</span>
+          </label>
+        </div>
+        <div className="signupBtn">가입하기</div>
       </form>
     </div>
   );
