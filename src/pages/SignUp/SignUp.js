d431e6ae1e41418477f318e6659a7e4f2dfc74f6
rayong <skdyds@gmail.com>
2022-09-23 18:56:47 +0900

    Add : 회원가입 백엔드 통신 테스트 - 아이디 비밀번호만 입력하고 나머진 고정값으로 입력 - 필수 입력값이 모두 있어야 통신이 전달됨

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.scss';
 
 function SignUp() {
+  const [userInput, setUserInput] = useState({
+    email: '',
+    pw: '',
+    pwCheck: '',
+  });
+
+  const handleInput = e => {
+    const { name, value } = e.target;
+    setUserInput({ ...userInput, [name]: value });
+  };
+
+  const isPwSame = userInput.pw === userInput.pwCheck ? true : false;
+
+  // const signupForm = document.getElementById('signupForm');
+
+  const checkSignUp = e => {
+    e.preventDefault();
+    fetch('https://8075-211-106-114-186.jp.ngrok.io/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email: userInput.email,
+        password: userInput.pw,
+        name: 'test6',
+        birthday: '1994-10-11',
+        phone_number: '010-3330-3333',
+        gender: 'man',
+      }),
+    });
+    // .then(response => {
+    //   if (response.ok === true) {
+    //     return response.json();
+    //   }
+    //   throw new Error('에러 발생!');
+    // })
+    // .catch(error => alert(error))
+    // .then(data => {
+    //   if (data.token) {
+    //     // localStorage.setItem('TOKEN', data.token);
+    //     alert('로그인 성공');
+    //     navigate('/rayong/Main');
+    //   } else {
+    //     alert('로그인 실패');
+    //   }
+    // });
+  };
+
   return (
     <div className="signUp">
       <form className="signUpBox">
@@ -14,13 +63,15 @@ function SignUp() {
         </div>
         {/* 이메일 비밀번호 입력 */}
         <input
+          onChange={handleInput}
           className="userInputEmail input"
-          name="id"
+          name="email"
           type="text"
           placeholder="이메일"
           autoComplete="username"
         />
         <input
+          onChange={handleInput}
           className="userInputPw input"
           name="pw"
           type="password"
@@ -28,12 +79,17 @@ function SignUp() {
           autoComplete="current-password"
         />
         <input
+          onChange={handleInput}
           className="userInputPwCheck input"
-          name="pw"
+          name="pwCheck"
           type="password"
           placeholder="비밀번호 확인"
           autoComplete="current-password"
         />
+        {!isPwSame && (
+          <p className="notSameMsg">비밀번호가 일치하지 않습니다.</p>
+        )}
+
         {/* 이름 입력 */}
         <p className="userName title mustInput">이름</p>
         <input
@@ -46,11 +102,11 @@ function SignUp() {
         {/* 성별 입력 */}
         <p className="userGender title mustInput">성별</p>
         <label className="userMale label">
-          <input className="radio" name="gender" type="radio" value="male" />
+          <input className="radio" name="gender" type="radio" value="man" />
           <span className="text">남자</span>
         </label>
         <label className="userFemale label">
-          <input className="radio" name="gender" type="radio" value="female" />
+          <input className="radio" name="gender" type="radio" value="woman" />
           <span className="text">여자</span>
         </label>
         {/* 휴대폰 입력 */}
@@ -67,9 +123,9 @@ function SignUp() {
           <p className="title mustInput">생년월일</p>
           <div className="selectBox">
             <select className="select" name="year">
-              <option value="1990">1990</option>
-              <option value="1991">1991</option>
-              <option value="1992">1992</option>
+              <option>1990</option>
+              <option>1991</option>
+              <option>1992</option>
             </select>
             <select className="select" name="month">
               <option value="1">1월</option>
@@ -103,7 +159,9 @@ function SignUp() {
             <span className="text">회원 탈퇴 시</span>
           </label>
         </div>
-        <div className="signupBtn">가입하기</div>
+        <div className="signupBtn" onClick={checkSignUp}>
+          가입하기
+        </div>
       </form>
     </div>
   );
