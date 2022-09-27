0197b4bbccc73956c2d55dcf22598f48f979d739
rayong <skdyds@gmail.com>
2022-09-27 18:38:16 +0900

    Modify : 백엔드 통신 테스트, api 고정 주소 전달

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -19,16 +19,16 @@ function ProductList() {
   // }, []);
 
   useEffect(() => {
-    fetch(`http://127.0.0.1:3000/category"?category=${category}&id=${id}`)
+    fetch(`http://192.168.47.96:3000/category/?category=main&id=2`)
       .then(res => res.json())
-      .then(res => setProducts(res));
+      .then(res => setProducts(res.getProducts));
   }, [category, id]);
 
   const sortColor = colorNum => {
     searchParams.set('color', colorNum);
     setSearchParams(searchParams);
     fetch(
-      `http://127.0.0.1:3000/category"?category=${category}&id=${id}&color=${color}`
+      `http://192.168.47.96:3000/category?category=${category}&id=${id}&color=${color}`
     )
       .then(res => res.json())
       .then(res => setProducts(res));
