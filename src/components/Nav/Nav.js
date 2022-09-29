3aba94e68c32e44e0dc22424a798b388d8e4f8d9
gamangee <gamangee1991@gmail.com>
2022-09-29 11:16:57 +0900

    수정사항 반영 완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -23,44 +23,31 @@ function Nav() {
     <nav className="navBox">
       <div className="nav">
         <ul className="menu">
-          <Link to="/">
-            <li className="li logo">WePleats</li>
-          </Link>
-          <Link to="/category/best">
-            <li className="li best">Best</li>
-          </Link>
-          <Link to="/category/new">
-            <li className="li new">New</li>
-          </Link>
-          <Link to="/category?category=main&id=1">
-            <li className="li monitor">Monitor</li>
-          </Link>
-          <Link to="/category?category=main&id=2">
-            <li className="li keyboard">Keyboard</li>
-          </Link>
-          <Link to="/category?category=main&id=3">
-            <li className="li mouse">Mouse</li>
-          </Link>
-          <Link to="/category?category=main&id=4">
-            <li className="li etc">Etc</li>
-          </Link>
+          {MENU_LIST.map(item => {
+            return (
+              <li key={item.id} className={item.className}>
+                <Link to={item.url}>{item.name}</Link>
+              </li>
+            );
+          })}
         </ul>
         <ul className="userInfo">
-          <Link to={userId ? null : '/signup'}>
-            <li className="li name">{userId ? userId : '+5000P'}</li>
-          </Link>
-
+          <li className="li name">
+            <Link to={userId ? null : '/signup'}>
+              {userId ? userId : '+5000P'}
+            </Link>
+          </li>
           <li className="li line" />
           <li className="li myPage">
             <i className="fa-solid fa-user" />
           </li>
           <li className="li line" />
-          <Link to="/cart">
-            <li className="li cart">
+          <li className="li cart">
+            <Link to="/cart">
               <i className="fa fa-shopping-bag" />
               {cartCount && <div className="count">{cartCount}</div>}
-            </li>
-          </Link>
+            </Link>
+          </li>
           <li className="li line" />
           <li className="li search">
             <i className="fa-solid fa-magnifying-glass" />
@@ -69,64 +56,48 @@ function Nav() {
       </div>
       <div className="navDropDown">
         <ul className="dropDown">
-          <Link to="/category/best">
-            <li className="li best">Best</li>
-          </Link>
-          <Link to="/category/new">
-            <li className="li new">New</li>
-          </Link>
-
+          <li className="li best">
+            <Link to="/category/best">Best</Link>
+          </li>
+          <li className="li new">
+            <Link to="/category/new">New</Link>
+          </li>
           <li className="li Monitor">
             <ul className="submenu">
-              <Link to="/category?category=sub&id=1">
-                <li className="sub">일반</li>
-              </Link>
-              <Link to="/category?category=sub&id=2">
-                <li className="sub">고해상도</li>
-              </Link>
-              <Link to="/category?category=sub&id=3">
-                <li className="sub">게이밍</li>
-              </Link>
-              <Link to="/category?category=sub&id=4">
-                <li className="sub">터치</li>
-              </Link>
+              {MONITOR_LIST.map(item => {
+                return (
+                  <li key={item.id} className="sub">
+                    <Link to={item.url}>{item.name}</Link>
+                  </li>
+                );
+              })}
             </ul>
           </li>
           <li className="li Keyboard">
             <ul className="submenu">
-              <Link to="/category?category=sub&id=5">
-                <li className="sub">유선</li>
-              </Link>
-              <Link to="/category?category=sub&id=6">
-                <li className="sub">무선</li>
-              </Link>
-              <Link to="/category?category=sub&id=7">
-                <li className="sub">블루투스</li>
-              </Link>
-              <Link to="/category?category=sub&id=8">
-                <li className="sub">기계식</li>
-              </Link>
+              {KEYBOARD_LIST.map(item => {
+                return (
+                  <li key={item.id} className="sub">
+                    <Link to={item.url}>{item.name}</Link>
+                  </li>
+                );
+              })}
             </ul>
           </li>
           <li className="li Mouse">
             <ul className="submenu">
-              <Link to="/category?category=sub&id=8">
-                <li className="sub">유선</li>
-              </Link>
-              <Link to="/category?category=sub&id=9">
-                <li className="sub">무선</li>
-              </Link>
-              <Link to="/category?category=sub&id=10">
-                <li className="sub">블루투스</li>
-              </Link>
-              <Link to="/category?category=sub&id=11">
-                <li className="sub">버티컬</li>
-              </Link>
+              {MOUSE_LIST.map(item => {
+                return (
+                  <li key={item.id} className="sub">
+                    <Link to={item.url}>{item.name}</Link>
+                  </li>
+                );
+              })}
             </ul>
           </li>
-          <Link to="/category?category=sub&id=12">
-            <li className="li etc">스티커</li>
-          </Link>
+          <li className="li etc">
+            <Link to="/category?category=sub&id=12">스티커</Link>
+          </li>
         </ul>
       </div>
     </nav>
@@ -134,3 +105,102 @@ function Nav() {
 }
 
 export default Nav;
+
+const MENU_LIST = [
+  { id: 1, name: 'WePleats', className: 'li logo', url: '/' },
+  { id: 2, name: 'Best', className: 'li best', url: '/category/best' },
+  { id: 3, name: 'New', className: 'li new', url: '/category/new' },
+  {
+    id: 4,
+    name: 'Monitor',
+    className: 'li monitor',
+    url: '/category?category=main&id=1',
+  },
+  {
+    id: 5,
+    name: 'Keyboard',
+    className: 'li keyboard',
+    url: '/category?category=main&id=2',
+  },
+  {
+    id: 6,
+    name: 'Mouse',
+    className: 'li mouse',
+    url: '/category?category=main&id=3',
+  },
+  {
+    id: 7,
+    name: 'etc',
+    className: 'li etc',
+    url: '/category?category=main&id=4',
+  },
+];
+
+const MONITOR_LIST = [
+  {
+    id: 1,
+    name: '일반',
+    url: '/category?category=sub&id=1',
+  },
+  {
+    id: 2,
+    name: '고해상도',
+    url: '/category?category=sub&id=2',
+  },
+  {
+    id: 3,
+    name: '게이밍',
+    url: '/category?category=sub&id=3',
+  },
+  {
+    id: 4,
+    name: '터치',
+    url: '/category?category=sub&id=4',
+  },
+];
+
+const KEYBOARD_LIST = [
+  {
+    id: 1,
+    name: '유선',
+    url: '/category?category=sub&id=5',
+  },
+  {
+    id: 2,
+    name: '무선',
+    url: '/category?category=sub&id=6',
+  },
+  {
+    id: 3,
+    name: '블루투스',
+    url: '/category?category=sub&id=7',
+  },
+  {
+    id: 4,
+    name: '기계식',
+    url: '/category?category=sub&id=8',
+  },
+];
+
+const MOUSE_LIST = [
+  {
+    id: 1,
+    name: '유선',
+    url: '/category?category=sub&id=9',
+  },
+  {
+    id: 2,
+    name: '무선',
+    url: '/category?category=sub&id=10',
+  },
+  {
+    id: 3,
+    name: '블루투스',
+    url: '/category?category=sub&id=11',
+  },
+  {
+    id: 4,
+    name: '버티컬',
+    url: '/category?category=sub&id=12',
+  },
+];
